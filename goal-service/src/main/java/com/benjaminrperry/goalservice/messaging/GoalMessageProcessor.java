aad54810a49b1d945466a6ad75fbba5c19040ed4
morimizu <neb_bbc@yahoo.com>
2023-09-15 12:14:05 -0300

    created goal-rest-gateway module fixes [8]

--- goal-service/src/main/java/com/benjaminrperry/goalservice/messaging/GoalMessageProcessor.java
+++ goal-service/src/main/java/com/benjaminrperry/goalservice/messaging/GoalMessageProcessor.java
@@ -5,6 +5,7 @@ import com.benjaminrperry.goalquest.api.goal.messaging.CompleteGoalMessage;
 import com.benjaminrperry.goalquest.api.goal.messaging.CreateGoalMessage;
 import com.benjaminrperry.goalquest.api.goal.messaging.GetGoalMessage;
 import com.benjaminrperry.goalquest.api.goal.messaging.GetGoalsMessage;
+import com.benjaminrperry.goalservice.converter.GoalConverter;
 import com.benjaminrperry.goalservice.service.GoalService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -17,6 +18,8 @@ import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Optional;
 
+import static com.benjaminrperry.goalservice.converter.GoalConverter.toGoalDTO;
+
 @RabbitListener(queues = "${goalquest.rabbit.bindings.goal.queue-name}")
 @Component
 @Slf4j
@@ -28,20 +31,27 @@ public class GoalMessageProcessor {
     @RabbitHandler
     public Goal handelCreateGoal(@Payload CreateGoalMessage createGoalMessage) {
         log.info("received new message: "+ createGoalMessage);
-        return goalService.createGoal(createGoalMessage.getDescription());
+        var goal = goalService.createGoal(createGoalMessage.getDescription());
+        return toGoalDTO(goal);
     }
 
     @RabbitHandler
     public Optional<Goal> handelGetGoal(@Payload GetGoalMessage getGoalMessage) {
-        return goalService.findGoalById(getGoalMessage.getGoalId());
+        return goalService.findGoalById(getGoalMessage.getGoalId())
+                .map(GoalConverter::toGoalDTO);
     }
 
     @RabbitHandler
     public List<Goal> handelGetGoals(@Payload GetGoalsMessage getGoalsMessage) {
         if(getGoalsMessage.getGoalId() != null) {
-            return List.of(goalService.findGoalById(getGoalsMessage.getGoalId()).orElseThrow(()-> new NoSuchElementException("cannot find goal")));
+            return List.of(goalService.findGoalById(getGoalsMessage.getGoalId())
+                            .map(GoalConverter::toGoalDTO)
+                    .orElseThrow(()-> new NoSuchElementException("cannot find goal")));
         }
-        return goalService.findAllGoals();
+        return goalService.findAllGoals().stream()
+                .map(GoalConverter::toGoalDTO)
+                .map(goal -> (Goal) goal)
+                .toList();
     }
 
     @RabbitHandler
---
+++ goalquest-rest-gateway/src/main/java/com/benjaminrperry/goalquest/GoalQuestRestGatewayApplication.java
@@ -0,0 +1,19 @@
+package com.benjaminrperry.goalquest;
+
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.reactive.config.EnableWebFlux;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.benjaminrperry.goalquest.controller", "com.benjaminrperry.messaging", "com.benjaminrperry.client"})
+@EnableWebFlux
+@EnableRabbit
+public class GoalQuestRestGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoalQuestRestGatewayApplication.class, args);
+    }
+
+}
